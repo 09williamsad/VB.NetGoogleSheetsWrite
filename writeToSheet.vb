9ef21ca903995665f859752462eccc38b824d81f
09williamsad <adamw1806@gmail.com>
2019-04-10 13:09:56 +0100

    Update writeToSheet.vb

--- writeToSheet.vb
+++ writeToSheet.vb
@@ -12,16 +12,16 @@ Imports Google.Apis.Sheets.v4.Data
 		
         Dim service = New SheetsService(New BaseClientService.Initializer() With {.HttpClientInitializer = credential, .ApplicationName = ApplicationName}) 'Create Google Sheets API service for connecting to the API.
         Dim spreadsheetId As String = "Spread sheet ID here"
-		Dim oblist = New List(Of Object)() From {"Data to write to cell"}
-		Dim valueRange As New ValueRange()
-		valueRange.MajorDimension = "COLUMNS"
-		valueRange.Values = New List(Of IList(Of Object))() From {oblist}
-		Dim subSheetID as String = "Subsheet ID here"
-		Dim range as String = subSheetID & "!A5"
-		Dim writeCellRowRequest As SpreadsheetsResource.ValuesResource.AppendRequest = service.Spreadsheets.Values.Append(valueRange, spreadsheetId, range)
-		If newRow = 1 Then 'For adding a new row if needed, sheets can only add a new row if the row it is adding at is an empty row, if not empty then it will add the new row to the next available empty row 
-			writeCellRowRequest.InsertDataOption = SpreadsheetsResource.ValuesResource.AppendRequest.InsertDataOptionEnum.INSERTROWS
-		End If 'If not adding new row then will overwrite cell
-		writeCellRowRequest.ValueInputOption = SpreadsheetsResource.ValuesResource.AppendRequest.ValueInputOptionEnum.RAW
-		writeCellRowRequest.Execute()
+	Dim oblist = New List(Of Object)() From {"Data to write to cell"}
+	Dim valueRange As New ValueRange()
+	valueRange.MajorDimension = "COLUMNS"
+	valueRange.Values = New List(Of IList(Of Object))() From {oblist}
+	Dim subSheetID as String = "Subsheet ID here"
+	Dim range as String = subSheetID & "!A5"
+	Dim writeCellRowRequest As SpreadsheetsResource.ValuesResource.AppendRequest = service.Spreadsheets.Values.Append(valueRange, spreadsheetId, range)
+	If newRow = 1 Then 'For adding a new row if needed, sheets can only add a new row if the row it is adding at is an empty row, if not empty then it will add the new row to the next available empty row 
+		writeCellRowRequest.InsertDataOption = SpreadsheetsResource.ValuesResource.AppendRequest.InsertDataOptionEnum.INSERTROWS
+	End If 'If not adding new row then will overwrite cell
+	writeCellRowRequest.ValueInputOption = SpreadsheetsResource.ValuesResource.AppendRequest.ValueInputOptionEnum.RAW
+	writeCellRowRequest.Execute()
 End Sub
